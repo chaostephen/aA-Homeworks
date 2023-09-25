1f96140d02494c3ad7d9ee2b75a1dc43e4098719
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-25 14:19:56 -0400

    added 23

--- Homework-9.22.23/movie_novice/queries.rb
+++ Homework-9.22.23/movie_novice/queries.rb
@@ -30,19 +30,40 @@
 
 def find_angelina
   # find Angelina Jolie by name in the actors table
-
+  instance.execute(<<-SQL, id)
+  SELECT
+    *
+  FROM
+    actors
+  WHERE
+    name='Angelina Jolie'
+  SQL
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-  
+  instance.execute(<<-SQL, id)
+  SELECT
+    title
+  FROM
+    movies
+  WHERE
+    score>=9
+  SQL
 end
 
 def star_wars
   # display the id, title, and year of each Star Wars movie in movies
   # hint: use 'select' and 'where'
-  
+  instance.execute(<<-SQL, id)
+  SELECT
+    id,title,yr
+  FROM
+    movies
+  WHERE
+    name in ('Star Wars')
+  SQL
 end
 
 
@@ -50,21 +71,50 @@ def below_average_years
   # display each year with movies scoring under 5, with the count of movies
   # scoring under 5 aliased as bad_movies, in descending order
   # hint: use 'select', 'where', 'group', 'order'
-  
+  instance.execute(<<-SQL, id)
+  SELECT
+    yr, COUNT(title) as bad_movies
+  FROM
+    movies
+  WHERE
+
+  GROUP BY
+    title
+  ORDER BY
+    score
+  SQL
 end
 
 def alphabetized_actors
   # display the id and name of the second 10 actor names (i.e., #s 11-20)
   # when ordered from A-Z
   # hint: use 'order' and 'limit'
-  
+  instance.execute(<<-SQL, id)
+  SELECT
+    id, name
+  FROM
+    actors
+  WHERE
+
+  SQL
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-  
+  instance.execute(<<-SQL, id)
+  SELECT
+    id, name
+  FROM
+    movies
+  JOIN castings
+  ON movies.id=castings.movie_id
+  JOIN actors
+  ON actors.id=castings.actor_id
+  WHERE
+    title = 'Pulp Fiction'
+  SQL
 end
 
 def uma_movies
